440e8986a13770e5095947db787edcfb3c6d9f7f
Chirag Jindal <chiragjindal2169@gmail.com>
2020-03-30 20:22:56 +0530

    Update login.php

--- login.php
+++ login.php
@@ -9,8 +9,8 @@ $_SESSION['config']['client_secret'] = 'f749144deeb15f23974fc28f1efa878c';
 $_SESSION['config']['api_endpoint'] = 'https://api.codechef.com/';
 $_SESSION['config']['authorization_code_endpoint'] = 'https://api.codechef.com/oauth/authorize';
 $_SESSION['config']['access_token_endpoint'] = 'https://api.codechef.com/oauth/token';
-$_SESSION['config']['redirect_uri'] = 'http://localhost/codechef/dashboard.php';
-$_SESSION['config']['website_base_url'] = 'http://localhost/codechef/index.html';
+$_SESSION['config']['redirect_uri'] = 'https://corona-se-darona.herokuapp.com/dashboard.php';
+$_SESSION['config']['website_base_url'] = 'https://corona-se-darona.herokuapp.com/index.html';
 
 $_SESSION['oauth_details'] = array();
 $_SESSION['oauth_details']['authorization_code'] = '';
